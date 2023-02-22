0fdb2521c7f8664e95c5a46d42361c7f5e79cd8b
scorpor2345 <117058280+scorpor2345@users.noreply.github.com>
2023-02-22 23:05:07 +0530

    Add files via upload

---
+++ directionstring.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class raj{
+public static float getStarted(String path){
+int x=0,y=0;
+for(int i=0;i<path.length();i++){
+char dir=path.charAt(i);
+if(dir=='s'){
+y--;
+}
+else if(dir=='n'){
+y++;
+}
+else if(dir=='w'){
+x--;
+}
+else{
+x++;
+}
+}
+int X2=x*x;
+int Y2=y*y;
+return(float)Math.sqrt(X2+Y2);
+
+}
+public static void main(String args[]){
+String path="eses";
+System.out.println(getStarted(path));
+}
+}
