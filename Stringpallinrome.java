ad33323414e8c3ffb4b1718fa7652f44fa537d81
scorpor2345 <117058280+scorpor2345@users.noreply.github.com>
2023-02-22 08:48:08 +0530

    Add files via upload

---
+++ Stringpallinrome.java
@@ -0,0 +1,17 @@
+class ArrayCC{
+public static boolean pallindrome(String str){
+for(int i=0;i<str.length()/2;i++){
+int n=str.length();
+if(str.charAt(i) != str.charAt(n-1-i)){
+return false;
+}
+}
+return true;
+}
+
+public static void main(String args[]){
+String str="bohb";
+
+System.out.println(pallindrome(str));
+}
+}
